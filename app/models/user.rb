b2575948b1862f86a150f6d3edc86b76678a5324
Takuya <dyhpaax@gmail.com>
2019-01-05 19:12:22 +0900

    kadai05

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15}
+
+
+  validates :email,presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  has_secure_password
+  validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i }
+end
