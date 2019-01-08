1f275399b2b65d3bb44a17fba91911469b3d70be
Takuya <dyhpaax@gmail.com>
2019-01-08 14:55:28 +0900

    07

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Pictgram
   class Application < Rails::Application
-  config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
-end
+    config.load_defaults 5.2
+    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
   end
+end
